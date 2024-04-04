91082252b7f83a7bb622063c4dbed410a2a5b99b
Saleha Khan <161796561+Ibrahimsabir@users.noreply.github.com>
2024-04-04 10:43:31 -0700

    Add files via upload

---
+++ todo.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+import chalk from "chalk"
+
+let friendName: string[]=[];
+
+let condition = true
+while (condition){
+
+let addName= await inquirer.prompt([
+    
+    {
+     name : "AddName",
+     type: "input",
+     message: "Do add your friend name"
+    },
+    {
+        name: "AddMore",
+        type: "confirm",
+        message: "Do you have any other friend to add?",
+        default: false
+    }
+    
+])
+friendName.push(addName.AddName)
+console.log(chalk.bgYellow(friendName))
+condition=addName.AddMore
+
+
+}
+
+friendName.map((names) => console.log(chalk.bgGreen(`\n\t ${names} , you are invited for dinner tonight \t\n`)))
